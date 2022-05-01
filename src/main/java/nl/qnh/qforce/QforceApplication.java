24ca776e8ec974916d1e70f5ba255ec8b4b54ccb
roelerps <roelerps@gmail.com>
2022-05-01 16:29:36 +0200

    Initial commit

---
+++ src/main/java/nl/qnh/qforce/QforceApplication.java
@@ -0,0 +1,13 @@
+package nl.qnh.qforce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QforceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QforceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/qnh/qforce/QforceApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.qnh.qforce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QforceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
